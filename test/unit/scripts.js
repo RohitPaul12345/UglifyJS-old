b189534c092e1ba4bb7bf50dd52a712b14ebb984
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-07-25 21:39:30 +0530

    Update scripts.js

--- test/unit/scripts.js
+++ test/unit/scripts.js
@@ -1,54 +1,54 @@
 global.DIGITS_OVERRIDE_FOR_TESTING = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$_0123456789";
 
-var fs = require('fs'),
+let fs = require('fs'),
 	uglify = require('../../uglify-js'),
 	jsp = uglify.parser,
 	nodeunit = require('nodeunit'),
 	path = require('path'),
 	pro = uglify.uglify;
 
-var Script = process.binding('evals').Script;
+let Script = process.binding('evals').Script;
 
-var scriptsPath = __dirname;
+let scriptsPath = __dirname;
 
 function compress(code) {
-	var ast = jsp.parse(code);
+	let ast = jsp.parse(code);
 	ast = pro.ast_mangle(ast, { mangle: true });
 	ast = pro.ast_squeeze(ast, { no_warnings: true });
         ast = pro.ast_squeeze_more(ast);
 	return pro.gen_code(ast);
 };
 
-var testDir = path.join(scriptsPath, "compress", "test");
-var expectedDir = path.join(scriptsPath, "compress", "expected");
+let testDir = path.join(scriptsPath, "compress", "test");
+let expectedDir = path.join(scriptsPath, "compress", "expected");
 
 function getTester(script) {
 	return function(test) {
-		var testPath = path.join(testDir, script);
-		var expectedPath = path.join(expectedDir, script);
-		var content = fs.readFileSync(testPath, 'utf-8');
-		var outputCompress = compress(content);
+		let testPath = path.join(testDir, script);
+		let expectedPath = path.join(expectedDir, script);
+		let content = fs.readFileSync(testPath, 'utf-8');
+		let outputCompress = compress(content);
 
 		// Check if the noncompressdata is larger or same size as the compressed data
 		test.ok(content.length >= outputCompress.length);
 
 		// Check that a recompress gives the same result
-		var outputReCompress = compress(content);
+		let outputReCompress = compress(content);
 		test.equal(outputCompress, outputReCompress);
 
 		// Check if the compressed output is what is expected
-		var expected = fs.readFileSync(expectedPath, 'utf-8');
+		let expected = fs.readFileSync(expectedPath, 'utf-8');
 		test.equal(outputCompress, expected.replace(/(\r?\n)+$/, ""));
 
 		test.done();
 	};
 };
 
-var tests = {};
+let tests = {};
 
-var scripts = fs.readdirSync(testDir);
-for (var i in scripts) {
-	var script = scripts[i];
+let scripts = fs.readdirSync(testDir);
+for (let i in scripts) {
+	let script = scripts[i];
 	if (/\.js$/.test(script)) {
 		tests[script] = getTester(script);
 	}
