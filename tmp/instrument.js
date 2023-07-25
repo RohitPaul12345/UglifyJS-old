9e66db762434af0d0da977bbf8c8a08f09f0d2d0
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-07-25 21:34:29 +0530

    Update instrument.js

--- tmp/instrument.js
+++ tmp/instrument.js
@@ -1,19 +1,19 @@
 // sample on how to use the parser and walker API to instrument some code
 
-var jsp = require("uglify-js").parser;
-var pro = require("uglify-js").uglify;
+let jsp = require("uglify-js").parser;
+let pro = require("uglify-js").uglify;
 
 function instrument(code) {
-        var ast = jsp.parse(code, false, true); // true for the third arg specifies that we want
+        let ast = jsp.parse(code, false, true); // true for the third arg specifies that we want
                                                 // to have start/end tokens embedded in the
                                                 // statements
-        var w = pro.ast_walker();
+        let w = pro.ast_walker();
 
         // we're gonna need this to push elements that we're currently looking at, to avoid
         // endless recursion.
-        var analyzing = [];
+        let analyzing = [];
         function do_stat() {
-                var ret;
+                let ret;
                 if (this[0].start && analyzing.indexOf(this) < 0) {
                         // without the `analyzing' hack, w.walk(this) would re-enter here leading
                         // to infinite recursion
@@ -31,7 +31,7 @@ function instrument(code) {
                 }
                 return ret;
         };
-        var new_ast = w.with_walkers({
+        let new_ast = w.with_walkers({
                 "stat"     : do_stat,
                 "label"    : do_stat,
                 "break"    : do_stat,
@@ -61,7 +61,7 @@ function instrument(code) {
 
 ////// test code follows.
 
-var code = instrument(test.toString());
+let code = instrument(test.toString());
 console.log(code);
 
 function test() {
@@ -71,7 +71,7 @@ function test() {
         "foo";
 
         // var
-        var foo = 5;
+        let foo = 5;
         const bar = 6, baz = 7;
 
         // switch block.  note we can't track case lines the same way.
@@ -83,15 +83,15 @@ function test() {
         }
 
         // for/for in
-        for (var i = 0; i < 5; ++i) {
+        for (let i = 0; i < 5; ++i) {
                 console.log("Hello " + i);
         }
-        for (var i in [ 1, 2, 3]) {
+        for (let i in [ 1, 2, 3]) {
                 console.log(i);
         }
 
         // note however that the following is broken.  I guess we
         // should add the block brackets in this case...
-        for (var i = 0; i < 5; ++i)
+        for (let i = 0; i < 5; ++i)
                 console.log("foo");
 }
